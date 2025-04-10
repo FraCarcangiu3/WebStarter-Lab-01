309bed3d6ff50039bff76d5e333999c92f93b151
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-04-10 17:10:56 +0200

    Update Main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from data.books import books
 app = FastAPI()
 
 @app.get("/books")
-def get_all_books() -> list[Book]:
+def get_all_books() -> list[Book]: #list[book] è il dato che
     """Restituisce la lista di tutti i libri disponibili"""
     return list(books.values()) #Mi restituisce i valori del dizionario books
