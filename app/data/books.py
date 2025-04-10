6754f7202ca15813d2901be001ad761cb510748b
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-04-10 17:42:41 +0200

    Update Main.py & book.py

--- app/data/books.py
+++ app/data/books.py
@@ -1,5 +1,6 @@
 from models.book import Book
 
+#finto database
 books = {
     0: Book(id=0, title="libro zero", author="autore zero", review=1),
     1: Book(id=1, title="libro uno", author="autore uno", review=2),
--- app/main.py
+++ app/main.py
@@ -8,3 +8,7 @@ app = FastAPI()
 def get_all_books() -> list[Book]: #list[book] è il dato che
     """Restituisce la lista di tutti i libri disponibili"""
     return list(books.values()) #Mi restituisce i valori del dizionario books
+
+if __name__ == "__main__": #è importante perchè esegue il codice che verrà dopo solo se eseguiamo questo file come script principale
+    import uvicorn #uvicorn è un server ASGI per eseguire applicazioni web
+    uvicorn.run(app, reload=True) #lanciamo l'applicazione FastAPI con il server uvicorn
