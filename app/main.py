13417174bf845b9855814093dab6f0155d66b1c1
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-04-13 23:54:26 +0200

    summary

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers import books
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 # === IMPORTAZIONE LIBRERIE PER IL FRONTEND ===
 # HTMLResponse: Classe che rappresenta una risposta HTTP in formato HTML
 from fastapi.responses import HTMLResponse
@@ -12,17 +13,23 @@ from fastapi import Request
 # StaticFiles: Per servire file statici come immagini, CSS e JavaScript
 from fastapi.staticfiles import StaticFiles
 =======
+=======
+>>>>>>> parent of 6c9e7c0 (update commit)
 #IMPORTO LA LIBRERIA FASTAPI PER CREARE UN'APPLICAZIONE WEB - parte frontend
 from fastapi.responses import HTMLResponse #HTMLResponse è una classe di FastAPI che rappresenta una risposta HTTP in formato HTML
 from fastapi.templating import Jinja2Templates #Jinja2Templates è una classe di FastAPI che gestisce i template HTML
 from fastapi import Request #Request è una classe di FastAPI che rappresenta una richiesta HTTP
 ###################################################
+<<<<<<< HEAD
+>>>>>>> parent of 6c9e7c0 (update commit)
+=======
 >>>>>>> parent of 6c9e7c0 (update commit)
 
 #IMPORTO LA LIBRERIA FASTAPI PER CREARE UN'APPLICAZIONE WEB - parte backend
 app = FastAPI()
 app.include_router(books.router, tags=["books"]) #includo il router per la gestione dei libri,
                                                  # tags è un modo per raggruppare le rotte in base alla loro funzionalità
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 # Configurazione per servire i file statici
@@ -33,6 +40,9 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 # Configurazione del sistema di template Jinja2, specificando la directory dove si trovano i file HTML
 templates = Jinja2Templates(directory="app/templates")
 =======
+>>>>>>> parent of 6c9e7c0 (update commit)
+=======
+
 >>>>>>> parent of 6c9e7c0 (update commit)
 
 #####PARTE FRONT END###################################################################################################
